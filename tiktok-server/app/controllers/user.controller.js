1216bdeac0d0fbe221202ddcd82c9792c6fc9113
vietnt94v <vietnt94v@gmail.com>
2022-03-18 17:48:30 +0700

    add serve nodejs

---
+++ tiktok-server/app/controllers/user.controller.js
@@ -0,0 +1,83 @@
+const User = require('../models/user.model.js')
+
+exports.create = (req, res) => {
+  if (!req.body) {
+    res.status(400).send({
+      message: 'Content can not be empty!'
+    })
+  }
+  const user = new User({
+    user_name: req.body.user_name,
+    password: req.body.password,
+    nikname: req.body.nikname,
+    description: req.body.description,
+    following: 0,
+    follower: 0,
+    favorite: 0,
+    status: 1
+  })
+  User.create(user, (err, data) => {
+    if (err)
+      res.status(500).send({
+        message: err.message || 'Some error occurred while creating the User.'
+      })
+    else res.send(data)
+  })
+}
+
+exports.findAll = (req, res) => {
+  const name = req.query.name
+  User.getAll(name, (err, data) => {
+    if (err)
+      res.status(500).send({
+        message: err.message || 'Some error occurred while retrieving users.'
+      })
+    else res.send(data)
+  })
+}
+exports.findAllActive = (req, res) => {
+  User.getAllActive((err, data) => {
+    if (err)
+      res.status(500).send({
+        message: err.message || 'Some error occurred while retrieving users.'
+      })
+    else res.send(data)
+  })
+}
+
+exports.findOne = (req, res) => {
+  User.findById(req.params.id, (err, data) => {
+    if (err) {
+      if (err.kind === 'not_found') {
+        res.status(404).send({
+          message: `Not found User with id ${req.params.id}.`
+        })
+      } else {
+        res.status(500).send({
+          message: 'Error retrieving User with id ' + req.params.id
+        })
+      }
+    } else res.send(data)
+  })
+}
+
+exports.update = (req, res) => {
+  if (!req.body) {
+    res.status(400).send({
+      message: 'Content can not be empty!'
+    })
+  }
+  User.updateById(req.params.id, new User(req.body), (err, data) => {
+    if (err) {
+      if (err.kind === 'not_found') {
+        res.status(404).send({
+          message: `Not found User with id ${req.params.id}.`
+        })
+      } else {
+        res.status(500).send({
+          message: 'Error updating User with id ' + req.params.id
+        })
+      }
+    } else res.send(data)
+  })
+}
---
+++ tiktok-server/app/routes/user.route.js
@@ -0,0 +1,21 @@
+module.exports = app => {
+  const users = require("../controllers/user.controller.js");
+  var router = require("express").Router();
+
+  // Create a new User
+  router.post("/", users.create);
+  // Retrieve all users
+  router.get("/", users.findAll);
+  // Retrieve all active users
+  router.get("/active", users.findAllActive);
+  // Retrieve a single User with id
+  router.get("/:id", users.findOne);
+  // Update a User with id
+  router.put("/:id", users.update);
+  // Delete a User with id
+  // router.delete("/:id", users.delete);
+  // Delete all users
+  // router.delete("/", users.deleteAll);
+  
+  app.use('/api/users', router);
+};
