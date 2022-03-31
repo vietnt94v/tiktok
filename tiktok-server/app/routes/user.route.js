0fa37f8abff4f40b6662c066a110f5979d90e5d6
vietnt94v <vietnt94v@gmail.com>
2022-03-31 18:15:49 +0700

    clear video server

--- tiktok-server/app/routes/user.route.js
+++ tiktok-server/app/routes/user.route.js
@@ -1,18 +1,12 @@
 module.exports = app => {
   const users = require('../controllers/user.controller.js')
-  const videos = require('../controllers/video.controller.js')
   var router = require('express').Router()
 
-  // User
   router.post('/user', users.create)
   router.get('/user', users.findAll)
   router.get('/user/active', users.findAllActive)
   router.get('/user/:id', users.findOne)
   router.put('/user/:id', users.update)
-
-  // Video
-  router.post('/video', videos.create)
-  router.get('/video', videos.getAll)
 
   // App user router link
   app.use('/api/v1/', router)
