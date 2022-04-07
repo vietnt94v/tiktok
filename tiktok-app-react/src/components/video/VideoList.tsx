938fa837020ca2c0d366388ab70477fc8fae02bb
vietnt94v <vietnt94v@gmail.com>
2022-04-07 10:54:02 +0700

    add event toggle play video

--- tiktok-app-react/src/components/video/VideoList.tsx
+++ tiktok-app-react/src/components/video/VideoList.tsx
@@ -1,7 +1,7 @@
-import { createRef, useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import VideoModel from '../../models/Video'
-import Video, { VideoExtendPlay } from '../../models/Video'
+import { VideoExtendPlay } from '../../models/Video'
 // import Video from './Video'
 import NavAction from './NavAction'
 import NavInfo from './NavInfo'
@@ -23,24 +23,12 @@ function VideoList({ videos, loadMoreVideo }: Props) {
     )
   }, [videos])
 
-  const handlePlayVideo = (index: number) => {
-    console.log(videosRefs.current[index])
-    videosRefs.current[index].play()
-  }
-
-  const handlePause = (index: number) => {
-    console.log('pause')
-    videosRefs.current[index].pause()
-  }
-
-  const handlePlay = (index: number) => {
-    console.log('play')
-    videosRefs.current[index].play()
-  }
-
-  const handleToggle = (index: number) => {
-    console.log('toggle')
-    videosRefs.current[index].play()
+  const handleToggleVideo = (video: VideoExtendPlay, index: number) => {
+    video.video_status_playing = !video.video_status_playing
+    setVideosExtendPlay([...videosExtendPlay])
+    video.video_status_playing
+      ? videosRefs.current[index].play()
+      : videosRefs.current[index].pause()
   }
 
   return (
@@ -48,41 +36,31 @@ function VideoList({ videos, loadMoreVideo }: Props) {
       className='w-full'
       direction={'vertical'}
       slidesPerView={1}
+      onSlideChange={() => {
+        console.log('slide change')
+      }}
       onReachEnd={() => loadMoreVideo()}
     >
-      {videos.map((video, index) => (
+      {videosExtendPlay.map((video, index) => (
         <SwiperSlide
           className='w-full h-full flex justify-center items-center'
           key={index}
         >
           <div className='relative flex items-center w-full h-full'>
             <video
+              loop
               ref={el => (videosRefs.current[index] = el)}
               src={video.video_src}
             ></video>
             <div
               className='absolute top-0 left-0 flex justify-center items-center w-full h-full cursor-pointer'
-              // onClick={() => handlePlayVideo(index)}
+              onClick={() => handleToggleVideo(video, index)}
             >
-              <i className='fa fa-play text-50 translate-x-2'></i>
-              <button
-                className='border-2 p-3 mx-5'
-                onClick={() => handlePause(index)}
-              >
-                Pause
-              </button>
-              <button
-                className='border-2 p-3'
-                onClick={() => handlePlay(index)}
-              >
-                Play
-              </button>
-              <button
-                className='border-2 p-3'
-                onClick={() => handleToggle(index)}
-              >
-                Toggle
-              </button>
+              {video.video_status_playing ? (
+                video.video_status_playing
+              ) : (
+                <i className='fa fa-play text-50 translate-x-2'></i>
+              )}
             </div>
             <div className='absolute bottom-5 right-2 z-50'>
               <NavAction action={video} />
